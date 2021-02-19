7edd714f608f5cdb96bec9f502e30518d6d9d429
kabir02091999 <kabir.02091999@gmail.com>
2021-02-19 01:04:55 -0400

    cambio malos

--- src/componet/Api.jsx
+++ src/componet/Api.jsx
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from 'react';
 import Axios from "axios"
 import Card from "./Card"
 import '../stylos/stylosCard.css'
+import Pagination from 'pagination-react-hooks';
+
 
 
 const Api = () => {
@@ -18,20 +20,50 @@ const Api = () => {
             console.log(error);
         });
     }, [setList]);
-     console.log(list) 
-    return ( <div className="container">
+     console.log(list)
+     
+    /* const llamar = (list) => {
 
-        {list.map(item=>(
+        list.map(data => (
            
-            <div key={item.id}>
+            <div key={data.id}>
                 
-                <Card parame={item}/>
+                <Card parame={data}/>
                 
-            </div>
+            </div>))
 
-        ))}
+    }
+ */
+    const show = (value) => (
+        <div className="container">
+        
+            <li key={value.id} className="card">
+                <span><img src={value.image}></img>
+                <h4>{value.id} - {value.name}</h4>
+                <p>status:{value.status}</p>
+                <p>species:{value.species}</p>
+                <p>origin:{value.origin.name}</p>
+                <p>location:{value.location.name}</p></span>         
+            </li>
+        
+        </div>
+      )
 
-    </div> );
+    return ( 
+        <div className="container">
+        
+            <Pagination
+            data={list}
+            Show={show}
+            displayNumber="5"
+            previousText="atras"
+            nextText="Sonraki"
+            />
+
+        </div>
+        
+
+     );
 }
  
 export default Api;
